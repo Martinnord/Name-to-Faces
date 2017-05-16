dc5db1ecdae5f8b879135ad6964d9163a753a0fe
martinnord <martin.nordstrom99@gmail.com>
2017-05-16 09:52:19 +0200

    new subclass created

---
+++ Name to Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Name to Faces
+//
+//  Created by Martin Nordström on 2017-05-16.
+//  Copyright © 2017 Martin Nordström. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
